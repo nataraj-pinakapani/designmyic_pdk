b98a1bb625ffc4d1025a240589731d63d5bcc6e4
nataraj-pinakapani <95837240+nataraj-pinakapani@users.noreply.github.com>
2022-10-22 13:48:04 +0100

    Update setup_install.sh
    Added csh install command

--- setup_install.sh
+++ setup_install.sh
@@ -1,5 +1,6 @@
 cd $HOME/Downloads
 sudo apt update
+sudo apt-get install csh
 sudo apt install python3-pip
 sudo pip3 install PySpice
 sudo pip3 install kneed
@@ -25,22 +26,8 @@ sudo make install
 
 cd ../
 git clone git://opencircuitdesign.com/open_pdks
-git clone https://github.com/google/skywater-pdk.git
-cd skywater-pdk
-
-git submodule init libraries/sky130_fd_io/latest
-git submodule init libraries/sky130_fd_pr/latest
-git submodule init libraries/sky130_fd_sc_hd/latest
-git submodule init libraries/sky130_fd_sc_hvl/latest
-#git submodule init libraries/sky130_fd_sc_hdll/latest
-#git submodule init libraries/sky130_fd_sc_hs/latest
-#git submodule init libraries/sky130_fd_sc_ms/latest
-#git submodule init libraries/sky130_fd_sc_ls/latest
-#git submodule init libraries/sky130_fd_sc_lp/latest
-git submodule update
-make timing 
 cd ../open_pdks
-./configure  --prefix=$HOME/projects/designmyic/cad/pdk/ --enable-sky130-pdk=$HOME/projects/designmyic/cad/pdk/downloads/skywater-pdk --enable-gf180mcu-pdk --with-sky130-variants=all --with-gf180mcu-variants=all --with-ef-style --enable-xschem-sky130 --enable-alpha-sky130 --enable-primitive-gf180mcu --enable-io-gf180mcu --enable-sc-7t5v0-gf180mcu  --enable-sc-9t5v0-gf180mcu --disable-sram-gf180mcu  --disable-sram-sky130
+./configure  --prefix=$HOME/projects/designmyic/cad/pdk/ --enable-sky130-pdk --enable-gf180mcu-pdk --with-sky130-variants=all --with-gf180mcu-variants=all --with-ef-style --enable-xschem-sky130 --enable-alpha-sky130 --enable-primitive-gf180mcu --enable-io-gf180mcu --enable-sc-7t5v0-gf180mcu  --enable-sc-9t5v0-gf180mcu --disable-sram-gf180mcu  --disable-sram-sky130
 
 make
 make install
@@ -48,7 +35,7 @@ make clean
 
 #INSTALL XSCHEM
 sudo apt update
-sudo apt install flex libx11-6 libx11-dev libxrender1 libxrender-dev
+sudo apt install flex libx11-6 libx11-dev libxrender1 libxrender-dev  
 sudo apt install libxcb1 libx11-xcb-dev libcairo2 libcairo2-dev
 sudo apt install tcl8.6 tcl8.6-dev tk8.6 tk8.6-dev flex bison
 sudo apt install libxpm4 libxpm-dev mawk gawk
@@ -79,37 +66,23 @@ git clone https://github.com/nataraj-pinakapani/cygnus.git
 cd  cygnus
 ./install.sh
 
-mkdir -p $HOME/projects/designmyic/cad/sky130_invoke
-cd $HOME/projects/designmyic/cad/sky130_invoke
-ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/sky130B/libs.tech/decred_hash_macro
-ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/sky130B/libs.tech/mips_cpu
-ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/sky130B/libs.tech/sky130_fd_pr
-ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/sky130B/libs.tech/sky130_stdcells
-ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/sky130B/libs.tech/stdcells
-ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/sky130B/libs.tech/xschem_verilog_import
-ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/sky130B/libs.tech/LICENSE
-ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/sky130B/libs.tech/README.md
-ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/sky130B/libs.tech/scripts
-ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/sky130B/libs.tech/sky130_fd_pr.patch
-ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/sky130B/libs.tech/sky130_tests
-ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/sky130B/libs.tech/xschemrc
-ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/sky130B/libs.tech/ngspice/spinit
+mkdir -p $HOME/projects/designmyic/cad/sky130_invoke/xschem
+cd $HOME/projects/designmyic/cad/sky130_invoke/xschem
 
-mkdir -p $HOME/projects/designmyic/cad/gf180mcu_invoke
-cd $HOME/projects/designmyic/cad/gf180mcu_invoke
-ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/sky130B/libs.tech/decred_hash_macro
-ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/sky130B/libs.tech/mips_cpu
-ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/sky130B/libs.tech/sky130_fd_pr
-ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/sky130B/libs.tech/sky130_stdcells
-ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/sky130B/libs.tech/stdcells
-ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/sky130B/libs.tech/xschem_verilog_import
-ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/sky130B/libs.tech/LICENSE
-ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/sky130B/libs.tech/README.md
-ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/sky130B/libs.tech/scripts
-ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/sky130B/libs.tech/sky130_fd_pr.patch
-ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/sky130B/libs.tech/sky130_tests
-ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/sky130B/libs.tech/xschemrc
-ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/sky130B/libs.tech/ngspice/spinit
 
-mkdir -p $HOME/projects/designmyic/cad/gf180mcu_invoke
-cd $HOME/projects/designmyic/cad/gf180mcu_invoke
+
+mkdir -p $HOME/projects/designmyic/cad/gf180mcu_invoke/xschem/simulations
+mkdir -p $HOME/projects/designmyic/cad/gf180mcu_invoke/xschem/sch_lib
+cd $HOME/projects/designmyic/cad/gf180mcu_invoke/xschem
+ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/gf180mcuC/libs.tech/xschem/symbols/
+ln -s symbols gf180mcu_fd_pr
+ln -s $HOME/projects/designmyic/cad/pdk/share/pdk/gf180mcuC/libs.tech/xschem/tests/
+ln -s tests gf180mcu_tests
+cp $HOME/projects/designmyic/cad/pdk/share/pdk/gf180mcuC/libs.tech/xschem/xschemrc .
+
+export PDK_ROOT=$HOME/projects/designmyic/cad/pdk/share/pdk
+
+ln -s design.ngspice design.spice
+ln -s sm141064.ngspice  sm141064.spice
+ln -s smbb000149.ngspice smbb000149.spice
+
